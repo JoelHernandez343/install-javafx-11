2c624a02483e4c0c52b10bfb9c35b4a0915c63fd
Joel Hernandez <joel.programador@gmail.com>
2020-02-20 00:26:46 -0600

    Fixed issue x2

--- install_intellij_community_2019.3.3.sh
+++ install_intellij_community_2019.3.3.sh
@@ -1,4 +1,4 @@
-#!bin/sh
+#!/usr/bin/env bash
 echo "Instructions from https://www.javahelps.com/2015/04/install-intellij-idea-on-ubuntu.html"
 echo "Please, create a launcher script!!!"
 wget https://download.jetbrains.com/idea/ideaIC-2019.3.3.tar.gz

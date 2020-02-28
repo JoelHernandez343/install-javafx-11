898d93ec49d0079a472068b135e5b50a77b72092
Joel Hernandez <joel.programador@gmail.com>
2020-02-27 21:32:24 -0600

    Minor fixes

--- install_intellij_community_2019.3.3.sh
+++ install_intellij_community_2019.3.3.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 echo "Instructions from https://www.javahelps.com/2015/04/install-intellij-idea-on-ubuntu.html"
-echo "Please, create a launcher script!!!"
+echo "Please, create a launcher script and restart your pc after installation!"
 mkdir ~/tmp
 cd ~/tmp
 wget https://download.jetbrains.com/idea/ideaIC-2019.3.3.tar.gz
 cd /opt/
 sudo tar -xvzf ~/tmp/ideaIC-2019.3.3.tar.gz
 /opt/idea-IC-193.6494.35/bin/idea.sh
-read -p "Do you want to delete the installer ideaIC-2019.3.3.tar.gz?" -n 1 -r
+read -p "Do you want to delete the installer ideaIC-2019.3.3.tar.gz? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
